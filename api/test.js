0e4f5d1375ec50735bc3235e9c4def14db34fdb6
Koushik <gk@Koushiks-Macbook-Air.local>
2026-04-14 16:14:06 +0530

    Remove debug endpoints

--- api/test.js
+++
@@ -1,7 +0,0 @@
-module.exports = async (req, res) => {
-  console.log("Test endpoint called");
-  res.status(200).json({
-    message: "Test endpoint works",
-    timestamp: new Date().toISOString()
-  });
-};
